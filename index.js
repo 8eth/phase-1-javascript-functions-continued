2f41a6f9c4465574f53bc3d5bc8fcc6531ad28f3
Betelihem <Betelihem>
2022-02-01 14:02:23 -0800

    commit tree fd65f3825a4dd615150b36d287d7a602f49f5c2b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (typeof (activity) === 'undefined') {
+        return 'This Saturday, I want to roller-skate!'
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+const mondayWork = function (activity) {
+    if (typeof (activity) === 'undefined') {
+        return 'This Monday, I will go to the office.'
+    } else {
+        return `This Monday, I will ${activity}.`
+    }
+}
+function wrapAdjective(param2 = '*') {
+    return function(param1 = 'special') {
+        return `You are ${param2}${param1}${param2}!`
+    }
+}
